ab6aeb90413ccf0095b5d11db1eaa35d86777c15
Zach <zbrennan613@gmail.com>
2017-12-10 12:03:37 -0500

    newline support

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -19,7 +19,7 @@ static uint16_t* terminal_buffer;
 void terminal_initialize(void) {
 	terminal_row = 0;
 	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	terminal_color = vga_entry_color(VGA_COLOR_GREEN, VGA_COLOR_BLACK);
 	terminal_buffer = VGA_MEMORY;
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
@@ -40,12 +40,18 @@ void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
 
 void terminal_putchar(char c) {
 	unsigned char uc = c;
-	terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
-	if (++terminal_column == VGA_WIDTH) {
-		terminal_column = 0;
-		if (++terminal_row == VGA_HEIGHT)
-			terminal_row = 0;
-	}
+    if (uc == '\n') {
+        terminal_row++;
+        terminal_column = 0;
+    }
+    else {
+    	terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
+    	if (++terminal_column == VGA_WIDTH) {
+    		terminal_column = 0;
+    		if (++terminal_row == VGA_HEIGHT)
+    			terminal_row = 0;
+    	}
+    }
 }
 
 void terminal_write(const char* data, size_t size) {
